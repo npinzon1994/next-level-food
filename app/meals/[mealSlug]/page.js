bba8180145d2ae209cac24723542eb556fdb3828
Nikki Pinzon <npinzon1994@gmail.com>
2024-04-14 15:43:49 -0400

    populated [mealSlug] page.js with data from database, based on that mealSlug.

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,6 +1,37 @@
-export default function MealDetailsPage({params}) {
-    return <main>
-        <h1>Meal Details</h1>
-        <p>{params.mealSlug}</p>
-    </main>
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+import Image from "next/image";
+
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  //formatting the instructions to include line breaks because
+  //line breaks are ignored when fetching from DB
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />"); //replacing its line breaks with HTML line breaks: <br />
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        {/*Need to do it this way because we modified the HTML code above the return statement*/}
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Page Not Found</h1>
+      <p>Oh no! We couldn't find the page for you! We're so sorry 😔</p>
+    </main>
+  );
+}
